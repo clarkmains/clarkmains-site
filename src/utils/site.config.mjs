13f5362310c9542b32060fc5a97c9cb34107636d
Clark Mains <clark.mains@gmail.com>
2022-09-14 16:01:28 +0100

    style: update favicon feat: first draft of migrated "about" page and required components

---
+++ src/utils/site.config.mjs
@@ -0,0 +1,34 @@
+export const SITE = {
+  author: "Clark Mains",
+  title: "Clark Mains | Cloud Engineer",
+  description: "Coding, DevOps and building on AWS.",
+  openGraphImage: {
+    alt: "Clark Mains | Cloud Engineer",
+    path: "/images/about/og-image.png",
+  },
+  favicon: {
+    type: "image/png",
+    path: "/favicon.png",
+  },
+  recentPosts: 2,
+  headline: "Hi, I'm Clark.",
+  byline:
+    "I'm a cloud engineer from Glasgow, Scotland. This is my blog - I write mostly about coding, DevOps and building things on AWS. Welcome!",
+  socials: [
+    {
+      label: "AWS Certified",
+      icon: "fa-brands:aws",
+      link: "https://www.youracclaim.com/users/clark-mains/badges",
+    },
+    {
+      label: "GitHub",
+      icon: "bi:github",
+      link: "https://github.com/clarkmains",
+    },
+    {
+      label: "LinkedIn",
+      icon: "bi:linkedin",
+      link: "https://linkedin.com/in/clarkmains",
+    },
+  ],
+};
